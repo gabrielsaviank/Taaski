74d048e911f5b609841ae73f9a77dce9e844706d
gabrielsavian <gabrielsvnzrd@gmail.com>
2022-01-25 10:09:43 -0300

    feat: migrated

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # README
 
-This is an App that I built for remembering Rails core concepts.
+This is a simple Task App, that I'm building to remember rails/ruby
+
+I made a very simple project, for more complex stuff check my Contracts Project.
 
 Things you may want to cover:
 
 * Ruby version _2.7.3_
 
-* System dependencies _Scaffold_
+* System dependencies NaN(yet)
 
 * Configuration
 
-* Database _SqLite_
+* Database _SqLite3_
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-  # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_181307) do
+ActiveRecord::Schema.define(version: 2022_01_25_125315) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -20,4 +20,14 @@ ActiveRecord::Schema.define(version: 2022_01_18_181307) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  add_foreign_key "tasks", "projects"
 end
