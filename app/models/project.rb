3a3ccfd478252b0a9e61774b5df3b738ac23d0ae
gabrielsavian <gabrielsvnzrd@gmail.com>
2022-01-25 09:49:58 -0300

    feat: added validator for empty fields

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   after_initialize :set_defaults
 
+  validates_presence_of :title, :description, :percent_complete
+
   scope :almost_completed, -> { where('percent_complete > 75.0') }
   scope :still_needs_some_work, -> { where('percent_complete < 75.0') }
 
